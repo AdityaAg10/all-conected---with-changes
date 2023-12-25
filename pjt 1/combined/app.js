bccd987864c794db74f38bb72e0c925dcb43544b
AdityaAg10 <aditya.agarwal493@gmail.com>
2023-12-25 16:17:28 +0530

    sixteen

--- pjt 1/combined/app.js
+++ pjt 1/combined/app.js
@@ -8,16 +8,47 @@ var distFromTop = horizontalSection.offsetTop;
 var scrollDistance = distFromTop + horizontalLength - windowWidth;
 
 // Set the height of #horizontal-section to horizontalLength
-horizontalSection.style.height = horizontalLength + 'px';
+horizontalSection.style.height = horizontalLength + "px";
 
-window.onscroll = function () {
+var char16 = document.querySelector(".char16");
+var char26 = document.querySelector(".char26");
+var elementOffset = horizontalSection.offsetTop;
+
+window.onscroll = function (e) {
   var scrollTopp = window.scrollY;
-  console.log(scrollTopp)
+  console.log(scrollTopp-elementOffset);
+  
   if (scrollTopp >= distFromTop && scrollTopp <= scrollDistance) {
     // Use translateX with the correct syntax
-    elementWrapper.style.transform = "translateX(-" + (scrollTopp - distFromTop) + "px)";
+    elementWrapper.style.transform =
+    "translateX(-" + (scrollTopp - distFromTop) + "px)";
+    
+    // Position char16 and char26 based on mouse position
+    console.log(elementOffset)
+    char16.style.top = scrollTopp + "px";
+    char26.style.top = scrollTopp + "px";
+    char16.style.left = scrollTopp-elementOffset + "px";
+    char26.style.left = scrollTopp-elementOffset + "px";
   }
-}
 
+  var charitty1 = document.querySelector("#charitty1");
+  charitty1.addEventListener("mousemove", function (event) {
+    char16.style.top = event.clientY + "px";
+    char16.style.left = scrollTopp-elementOffset + "px";
 
+    char16.style.display = "block";
+  });
+  charitty1.addEventListener("mouseleave", function () {
+    char16.style.display = "none";
+  });
 
+  var charitty2 = document.querySelector("#charitty2");
+  charitty2.addEventListener("mousemove", function (event) {
+    char26.style.top = event.clientY + "px";
+    char26.style.left = scrollTopp-elementOffset + "px";
+    char26.style.display = "block";
+  });
+  charitty2.addEventListener("mouseleave", function () {
+    char26.style.display = "none";
+  });
+};
